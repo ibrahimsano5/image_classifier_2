be4cbcf5a089e1bd4047719048ccafb2f8ba2c18
Ibrahim Sano <sanoibrahim15@gmail.com>
2020-08-21 01:30:35 +0200

    Revised commit (bis)

--- predict.py
+++ predict.py
@@ -1,22 +1,93 @@
-import numpy as np
-import argparse
-import json
 import torch
-from PIL import Image
+from torch import optim, nn, tensor
+from torch.optim import lr_scheduler
+import torchvision
 from torchvision import datasets, transforms, models
+import json
+from collections import OrderedDict
+import PIL
+from PIL import Image
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import copy
+import argparse
+import utility
 
 parser = argparse.ArgumentParser()
-parser = argparse.ArgumentParser()
-parser.add_argument("image_path", action="store", type = str)
-parser.add_argument("save_checkpoint", action="store", type = str)
-parser.add_argument("--category_names", default="cat_to_name.json", action="store")
-parser.add_argument("--gpu", default=False, action="store_true")
+parser.add_argument("--image", default="image_path", type=str)
+parser.add_argument("--checkpoint", default="save_checkpoint.pth",type=str)
 parser.add_argument("--top_k", default=5, type=int)
+parser.add_argument("--category_names", default='cat_to_name.json', action="store")
+parser.add_argument("--gpu", default=False, action="store_true")
 results = parser.parse_args()
 
+#All saved data were included in utility.py and in checkpoint, but there's a problem when calling previous functions by importing datas, so
+#I copied and pasted them back here, but the program without these is the right one. Copied data (copied from train.py) start from "#copied"
+#to "#pasted"
+
+
+#Copied --->
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+data_transforms = { 
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(size=224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485,0.456,0.406],
+                             [0.229,0.224,0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485,0.456,0.406],
+                             [0.229,0.224,0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(size=256),
+        transforms.CenterCrop(size=224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485,0.456,0.406],
+                             [0.229,0.224,0.225])
+    ])
+}
+image_datasets = {
+    'train': datasets.ImageFolder(train_dir, transform=data_transforms['train']),
+    'valid': datasets.ImageFolder(valid_dir, transform=data_transforms['valid']),
+    'test': datasets.ImageFolder(test_dir, transform=data_transforms['test'])
+}
+dataloaders = {
+    'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True),
+    'valid': torch.utils.data.DataLoader(image_datasets['valid'], batch_size=64, shuffle=True),
+    'test': torch.utils.data.DataLoader(image_datasets['test'], batch_size=64, shuffle=True)
+}
+
+model = models.vgg16(pretrained=True)
+model
+for param in model.parameters():
+    param.requires_grad = False
+classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(25088, 512)),
+        ('ReLu1', nn.ReLU()),
+        ('Dropout1', nn.Dropout(0.05)),
+        ('fc3', nn.Linear(512, 102)),
+        ('output', nn.LogSoftmax(dim=1))]))
+print('Utility imported')
+#<--- Pasted
+
+model.classifier = classifier
+model.class_to_idx = image_datasets['train'].class_to_idx
+
 def load_checkpoint(path="save_checkpoint.pth"):
-    checkpoint = torch.load(filepath)
+    checkpoint = torch.save(save_checkpoint, path)
     
     model = checkpoint['model']
     
@@ -28,10 +99,18 @@ def load_checkpoint(path="save_checkpoint.pth"):
     optimizer.load_state_dict(checkpoint['optimizer_state_dic'])
     
     return model
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    process_image = transforms.Compose([ transforms.Resize(255), transforms.CenterCrop(224), 
+                                         transforms.ToTensor(), 
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
     
+    picture = process_image(Image.open(image))
     
-    
-    return model
+    return picture
 
 def predict(image_path, model, topk=5):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
@@ -55,3 +134,9 @@ def predict(image_path, model, topk=5):
         classes.append(idx_to_class[idx])
     
     return list(top_k), list(classes)
+
+image_path = "flowers/test/33/image_06486.jpg"
+probs, classes = predict(image_path, model)
+print(probs)
+print(classes)
+print("Prediction made, end of Part 2.")
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import torch
-import argparse
 from torch import optim, nn, tensor
 from torch.optim import lr_scheduler
 import torchvision
@@ -14,19 +13,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import copy
+import argparse
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument("--data_dir", default="./flowers", action='store', help='Specify the image data directory')
-parser.add_argument("--save_dir", default = "./save_checkpoint.pth", action="store")
-parser.add_argument("--learning_rate", default=0.01, type=float)
-parser.add_argument("--arch", default="vgg16", type = str)
-parser.add_argument("--epochs", default=20, type=int)
+parser.add_argument("--data_dir", default="./flowers", action="store")
+parser.add_argument("--save_dir", default="./save_checkpoint.pth", action="store")
+parser.add_argument("--learning_rate", default=0.01, type=float, action="store")
+parser.add_argument("--arch", default="vgg16", type = str, action="store")
+parser.add_argument("--epochs", default=3, type=int, action="store")
 parser.add_argument("--gpu", default=False, action="store_true")
-parser.add_argument('--hidden_layers', type=int, default=512, help='Specify the hidden units of your model')
+parser.add_argument("--hidden_layers", default=512, type=int, action="store")
 results = parser.parse_args()
 
-data_dir = "flowers"
+data_dir = 'flowers'
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 test_dir = data_dir + '/test'
@@ -54,19 +53,43 @@ data_transforms = {
                              [0.229,0.224,0.225])
     ])
 }
-
 image_datasets = {
     'train': datasets.ImageFolder(train_dir, transform=data_transforms['train']),
     'valid': datasets.ImageFolder(valid_dir, transform=data_transforms['valid']),
     'test': datasets.ImageFolder(test_dir, transform=data_transforms['test'])
 }
-
 dataloaders = {
     'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True),
     'valid': torch.utils.data.DataLoader(image_datasets['valid'], batch_size=64, shuffle=True),
     'test': torch.utils.data.DataLoader(image_datasets['test'], batch_size=64, shuffle=True)
 }
 
+print(dataloaders['train'])
+print(dataloaders['valid'])
+print(dataloaders['test'])
+
+model = models.vgg16(pretrained=True)
+for param in model.parameters():
+    param.requires_grad = False
+classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(25088, 512)),
+        ('ReLu1', nn.ReLU()),
+        ('Dropout1', nn.Dropout(0.05)),
+        ('fc3', nn.Linear(512, 102)),
+        ('output', nn.LogSoftmax(dim=1))]))
+
+model.classifier = classifier
+
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+
+cuda = torch.cuda.is_available()
+
+if cuda:
+    model.cuda()
+else:
+    model.cpu()
+    
 epochs = 3
 print_every = 5
 steps = 0
@@ -80,10 +103,9 @@ for epoch in range(epochs):
         steps += 1
         inputs, labels = Variable(images), Variable(labels)
         optimizer.zero_grad()
-
+        
         if cuda:
             inputs, labels = inputs.cuda(), labels.cuda()
-        
         output = model.forward(inputs)
         loss = criterion(output, labels)
         loss.backward()
@@ -134,4 +156,4 @@ save_checkpoint = {"class_to_idx" : model.class_to_idx,
                    "epochs" : 3
 }
 torch.save(save_checkpoint, 'save_checkpoint.pth')
-print(model.state_dict().keys())
+print("Model has been trained and saved.")
